8f5dd3e88d9ca446bde0fb14a81485775141ec3c
Yana <anazigaluk@gmail.com>
2024-12-04 11:15:22 +0200

    make build

---
+++ index.6cde9553.js
@@ -0,0 +1,2 @@
+!function e(n,t){var r=document.createElement("ul");return Object.keys(t).forEach(function(n){var a=document.createElement("li");a.textContent=n,r.appendChild(a),Object.keys(t[n]).length>0&&e(a,t[n])}),n.appendChild(r),r}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.6cde9553.js.map
